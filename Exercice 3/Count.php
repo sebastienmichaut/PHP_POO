4c3882b8058d80d308c8f0869d6dd3eb5dca6705
Sébastien Michaut <sebmichaut00@gmail.com>
2021-05-19 11:21:20 +0200

    nouveau TP

--- Exercice 3/Count.php
+++ Exercice 3/Count.php
@@ -3,6 +3,16 @@
 class Count{
     private static $count = 0;
 
-public function __construct($count){
-    self::$count;
+    public function __construct() {
+        self::$count++;
+    }
+
+    public static function affichage(){
+        echo self::$count;
+    }
 }
+
+$nouveau = new Count();
+$nouveau2 = new Count();
+$nouveau3 = new Count();
+Count::affichage();
